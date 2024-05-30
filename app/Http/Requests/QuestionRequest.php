bead525adca971d6e4798182800f9e1109da6001
panha <sokcheapanha11@gmail.com>
2024-05-30 16:11:24 +0700

    fixed

--- app/Http/Requests/QuestionRequest.php
+++ app/Http/Requests/QuestionRequest.php
@@ -12,7 +12,7 @@ class QuestionRequest extends FormRequest
             'name' => 'required|string|lowercase',
             'category_id' => 'required|integer',
             'level_id' => 'required|integer',
-            'is_graduate' => 'required',
+            'is_graduate' => 'required|integer',
             'choices' => 'required|array|min:3|max:3',
             'choices.*.name' => 'required|string|lowercase',
             'choices.*.is_correct' => 'required',
